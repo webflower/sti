c88518528c790e8554e7f7a68c588587b56f4ef0
webflower <https://github.com/webflower/webflower.github.io.git>
2019-05-23 13:51:44 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -38,11 +38,11 @@ $(document).ready(function(){
         },
         on: {
             slideChangeTransitionStart: function () {
-            $('.text-box h2, .text-box p').hide(0);
-            $('.text-box h2, .text-box p').removeClass('aos-init').removeClass('aos-animate');
+            $('.text-box').hide(0);
+            $('.text-box').removeClass('aos-init').removeClass('aos-animate');
         },
             slideChangeTransitionEnd: function () {
-                $('.text-box h2, .text-box p').show(0);
+                $('.text-box').show(0);
                 AOS.init();
             }
         }
