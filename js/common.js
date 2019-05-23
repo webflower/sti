06a07024f2f43f01e50ad5ed3a04db2c077b36c4
webflower <https://github.com/webflower/webflower.github.io.git>
2019-05-23 11:33:53 +0900

    마크업수정

--- js/common.js
+++ js/common.js
@@ -3,18 +3,22 @@ $(document).ready(function(){
 
     var headerMenuList = $(".header-menu a");
     var subcategory = $(".subcategory");
-
+    var header = $("header");
+    var nav = $("nav");
     //헤더 메뉴
     headerMenuList.mouseenter(function(){
         headerMenuList.removeClass("on");
         subcategory.css({
             opacity : 1
-        }).stop().slideDown();
+        });
+        header.addClass("hover").stop().slideDown();
         $(this).addClass("on");
-    }).mouseleave(function(){
+    });
+    nav.mouseleave(function(){
             $(".subcategory").css({
                 opacity : 0
-            }).stop().slideUp();
+            });
+            header.remove("hover").stop().slideUp();
             $(this).removeClass("on");
 
     });
