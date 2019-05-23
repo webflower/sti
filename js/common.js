b39e5964d8ccc19ef2e91b25dabb8a32b89f6fe9
webflower <https://github.com/webflower/webflower.github.io.git>
2019-05-23 17:18:32 +0900

    test

--- js/common.js
+++ js/common.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
         //     delay: 2500,
         //     disableOnInteraction: false,
         // },
-        speed : 700,
+        speed : 400,
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
