42010da6b605723f53c48236b36a9278ded5d941
webflower <https://github.com/webflower/webflower.github.io.git>
2019-05-23 11:47:24 +0900

    no

--- js/common.js
+++ js/common.js
@@ -9,26 +9,18 @@ $(document).ready(function(){
     headerMenuList.mouseenter(function(){
         headerMenuList.removeClass("on");
         subcategory.css({
-            opacity : 1,
-            display: "block"
-        });
-        header.addClass("hover").animate(500);
+            opacity : 1
+        }).stop().slideDown(800);
+        header.addClass("hover");
         $(this).addClass("on");
     });
     nav.mouseleave(function(){
             $(".subcategory").css({
-                opacity : 0,
-                display: "none"
-            });
-            header.removeClass("hover").animate(500);
+                opacity : 0
+            }).stop().slideUp(800);
+            header.removeClass("hover");
             $(this).removeClass("on");
 
     });
-    // subcategory.mouseleave(function(){
-
-    // })
-
-    // $()
-    
 
 })
